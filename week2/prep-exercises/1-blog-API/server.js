e8bfbb78088f7d2bb1c2b4809b3a8ff869be7de1
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-02-03 14:52:26 +0100

    updated server.js

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,23 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
+const fs = require("fs");
+app.use(express.json());
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+
+
+app.post("/blogs", (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ error: "Title and content are required" });
+  }
+  fs.writeFileSync(title, content);
+  res.send("ok");
+});
+
+app.listen(3000);
