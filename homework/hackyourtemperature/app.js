60a0252f5796e55cafecf1891d6da111d1cb3266
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-02-10 16:06:12 +0100

    tests added

---
+++ homework/hackyourtemperature/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import fetch from "node-fetch";
+import keys from "./sources/keys.js";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend to frontend!");
+});
+
+app.post("/weather", async (req, res) => {
+  const { cityName } = req.body;
+
+  if (!cityName) {
+    res.status(400).send("Please provide a valid cityName");
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${keys.API_KEY}`
+    );
+    const data = await response.json();
+
+    if (data.cod === "200") {
+      const temperature = data.list[0].main.temp;
+
+      res.send({ weatherText: `Weather in ${cityName}: ${temperature}K` });
+    } else {
+      res.send({ weatherText: "City is not found!" });
+    }
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export default app;
--- homework/hackyourtemperature/tests/app.test.js
+++ homework/hackyourtemperature/tests/app.test.js
@@ -1,10 +1,45 @@
+import request from "supertest";
+import app from "../app.js";
 
-
-describe("POST /", () => {
-  it("Quick test", () => {
-    expect(1).toBe(1);
+describe("GET /", () => {
+  it('responds with "Hello from backend to frontend!"', async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello from backend to frontend!");
   });
 });
+
+describe("POST /weather", () => {
+  it("responds with weather information when valid cityName is provided", async () => {
+    const cityName = "London";
+    const response = await request(app).post("/weather").send({ cityName });
+
+    expect(response.status).toBe(200);
+    expect(response.body.weatherText).toBeDefined();
+  });
+
+  it("responds with an error when no cityName is provided", async () => {
+    const response = await request(app).post("/weather");
+    expect(response.status).toBe(400);
+    expect(response.text).toBe("Please provide a valid cityName");
+  });
+});
+
+// it('responds with an error when an invalid cityName is provided', async () => {
+//   const cityName = 'InvalidCityName';
+//   const response = await request(app)
+//     .post('/weather')
+//     .send({ cityName });
+
+//   expect(response.status).toBe(200);
+//   expect(response.body.weatherText).toBe('City is not found!');
+// });;
+
+// describe("POST /", () => {
+//   it("Quick test", () => {
+//     expect(1).toBe(1);
+//   });
+// });
 
 // describe("GET /", () => {
 //   it('responds with "Hello from backend to frontend!"', async () => {
