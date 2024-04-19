477218a973c96486855c8721cf977a280842f62f
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-19 13:12:58 +0200

    changes applied according to feedback

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
-import keys from "./sources/keys.js";
+import { keys } from "./sources/keys.js";
 const app = express();
 const PORT = 4000;
 
@@ -15,21 +15,23 @@ app.post("/weather", async (req, res) => {
 
   if (!cityName) {
     res.status(400).send("Please provide a valid cityName");
+    return;
   }
 
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${keys.API_KEY}`
+      `${keys.BASE_URL}/weather?q=${cityName}&appid=${keys.API_KEY}`
     );
-    const data = await response.json();
 
-    if (data.cod === "200") {
-      const temperature = data.list[0].main.temp;
-
-      res.send({ weatherText: `Weather in ${cityName}: ${temperature}K` });
-    } else {
-      res.send({ weatherText: "City is not found!" });
+    if (!response.ok) {
+      res.status(response.status).send({ weatherText: "City is not found!" });
+      return;
     }
+
+    const {
+      main: { temp },
+    } = await response.json();
+    res.send({ weatherText: `Weather in ${cityName}: ${temp}K` });
   } catch (error) {
     console.error("Error fetching weather data:", error);
     res.status(500).send("Internal Server Error");
--- homework/hackyourtemperature/sources/keys.js
+++ homework/hackyourtemperature/sources/keys.js
@@ -1,6 +1,5 @@
-const keys = {
-  API_KEY: "6527189f01ec60d474ad747c2e141452",
+export const keys = { 
+  API_KEY: 'ddcd5971c6039bbcee33f5aa0f819234',
+  BASE_URL: 'https://api.openweathermap.org/data/2.5'
 };
-
-export default keys;
 
