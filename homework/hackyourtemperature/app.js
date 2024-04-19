7172b8999e8bd4ceca0f9215c92c4a5db22351f5
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-04-19 13:31:15 +0200

    feedback applied

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import fetch from "node-fetch";
-import keys from "./sources/keys.js";
+import { keys } from "./sources/keys.js";
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,22 +13,24 @@ app.post("/weather", async (req, res) => {
   const { cityName } = req.body;
 
   if (!cityName) {
-    return res.status(400).send("Please provide a valid cityName");
+    res.status(400).send("Please provide a valid cityName");
+    return;
   }
 
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${keys.API_KEY}`
+      `${keys.BASE_URL}/weather?q=${cityName}&appid=${keys.API_KEY}`
     );
-    const data = await response.json();
 
-    if (data.cod === 200) {
-      const temperature = data.list[0].main.temp;
-      res.send({ weatherText: `Weather in ${cityName}: ${temperature}K` });
-    } else {
-      res.send({ weatherText: "City is not found!" });
+    if (!response.ok) {
+      res.status(response.status).send({ weatherText: "City is not found!" });
       return;
     }
+
+    const {
+      main: { temp },
+    } = await response.json();
+    res.send({ weatherText: `Weather in ${cityName}: ${temp}K` });
   } catch (error) {
     console.error("Error fetching weather data:", error);
     res.status(500).send("Internal Server Error");
